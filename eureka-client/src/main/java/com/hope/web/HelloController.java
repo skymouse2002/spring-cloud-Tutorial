38b93bb5a8dfb893a92d83d58b7a8e974b7db880
kevin <hao_3602@163.com>
2018-04-20 18:22:56 +0800

    eureka client

---
+++ eureka-client/src/main/java/com/hope/web/HelloController.java
@@ -0,0 +1,25 @@
+package com.hope.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "hello", method = RequestMethod.GET)
+    public String index(@RequestParam String name) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        logger.info("/hello, host:" + instance.getHost() + ", service_id:" + instance.getServiceId());
+        return "Hello World " +instance.getHost()+" "+name;
+    }
+}
